eafefa28644d582525ae788f001ff7e549303823
Dalvinder Singh <helly.techup@gmail.com>
2022-12-27 13:22:52 +0530

    Create Project API Updates

--- PieceTracker.API/Controllers/ProjectSummaryMasterAPIController.cs
+++ PieceTracker.API/Controllers/ProjectSummaryMasterAPIController.cs
@@ -19,13 +19,15 @@ namespace PieceTracker.API.Controllers
     {
         private readonly ILoggerManager _logger;
         private IProjectSummaryMasterService _roleService;
+        private IProjectItemsMasterService _itemsMasterService;
         private IConfiguration _config;
         private readonly ApplicationSettings _appSettings;
 
-        public ProjectSummaryMasterAPIController(ILoggerManager logger, IProjectSummaryMasterService roleService, IConfiguration config, IOptions<ApplicationSettings> appSettings)
+        public ProjectSummaryMasterAPIController(ILoggerManager logger, IProjectSummaryMasterService roleService, IProjectItemsMasterService itemsMasterService, IConfiguration config, IOptions<ApplicationSettings> appSettings)
         {
             _logger = logger;
             _roleService = roleService;
+            _itemsMasterService = itemsMasterService;
             _config = config;
             _appSettings = appSettings.Value;
         }
@@ -115,6 +117,74 @@ namespace PieceTracker.API.Controllers
             {
                 _logger.Information(ex.ToString());
                 response.Success = false;
+                response.Message = ex.Message;
+                throw;
+            }
+        }
+        [HttpPost("createupdateProject")]
+        public async Task<BaseApiResponse> CreateProjectDetail([FromBody] AddUpdateProjectRequest request)
+        {
+            BaseApiResponse response = new BaseApiResponse();
+            try
+            {
+                AddUpdateProjectSummaryRequest summaryRequest = new AddUpdateProjectSummaryRequest
+                {
+                    Id = request.Id,
+                    ProjectName = request.ProjectName,
+                    ContractAmount = request.ContractAmount,
+                    CustomerName = request.CustomerName,
+                    EstimatedHours = request.EstimatedHours,
+                    EstimatedCost = request.EstimatedCost,
+                    SiteAddress = request.SiteAddress
+                };
+                var result = await _roleService.AddUpdateRecord(summaryRequest);
+                if (result.Status)
+                {
+                    AddUpdateProjectItemsMasterRequest itemsMasterRequest = new AddUpdateProjectItemsMasterRequest
+                    {
+                        Id = request.ItemMasterId,
+                        PartNumber = request.PartNumber,
+                        Description = request.Description,
+                        ProjectId = request.Id,
+                        Quantity = request.Quantity,
+                        Weight = request.Weight,
+                        ProjectLocation = request.ProjectLocation,
+                        BatchNumberProd = request.BatchNumberProd,
+                        ItemType = request.ItemType,
+                        SubItemType = request.SubItemType,
+                        EstimateLabourHours = request.EstimateLabourHours,
+                        ReferSheetNumber = request.ReferSheetNumber,
+                        CoatingType = request.CoatingType,
+                        ScheduleFabricateDate = request.ScheduleFabricateDate,
+                        ScheduleCoatingDate = request.ScheduleCoatingDate,
+                        ActualFabricateDate = request.ActualFabricateDate,
+                        ActualCoatingDate = request.ActualCoatingDate,
+                        FabricatorName = request.FabricatorName,
+                        CoatingVendorName = request.CoatingVendorName,
+                        ItemStatus = request.ItemStatus
+                    };
+                    var result1 = _itemsMasterService.AddUpdateRecord(itemsMasterRequest);
+                    response.Id = result.Id;
+                    response.Message = result.Message;
+                    response.StatusCode = HttpStatusCode.OK;
+                    response.Success = result.Status;
+                }
+                else
+                {
+                    var delResult = _roleService.DeleteRecord(summaryRequest);
+                    response.Id = result.Id;
+                    response.Message = "Error in creating a project.";
+                    response.StatusCode = HttpStatusCode.BadRequest;
+                    response.Success = false;
+                }
+                
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.Information(ex.ToString());
+                response.Success = false;
+                response.StatusCode = HttpStatusCode.BadRequest;
                 response.Message = ex.Message;
                 throw;
             }
---
+++ PieceTracker.Model/Request/AddUpdateProjectRequest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PieceTracker.Model
+{
+    public class AddUpdateProjectRequest : RequestBaseModel
+    {
+        public int ItemMasterId { get; set; }
+        public string ProjectName { get; set; }
+        public decimal ContractAmount { get; set; }
+        public string CustomerName { get; set; }
+        public decimal EstimatedHours { get; set; }
+        public decimal EstimatedCost { get; set; }
+        public string SiteAddress { get; set; }
+        public string PartNumber { get; set; }
+        public string Description { get; set; }
+        public int DeliveryId { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Weight { get; set; }
+        public string ProjectLocation { get; set; }
+        public string BatchNumberProd { get; set; }
+        public int ItemType { get; set; }
+        public int SubItemType { get; set; }
+        public decimal EstimateLabourHours { get; set; }
+        public string ReferSheetNumber { get; set; }
+        public int CoatingType { get; set; }
+        public DateTime ScheduleFabricateDate { get; set; }
+        public DateTime ScheduleCoatingDate { get; set; }
+        public DateTime ActualFabricateDate { get; set; }
+        public DateTime ActualCoatingDate { get; set; }
+        public string FabricatorName { get; set; }
+        public string CoatingVendorName { get; set; }
+        public int ItemStatus { get; set; }
+        public string ItemQRCode { get; set; }
+    }
+}
